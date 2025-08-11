87232c249c4e5ffe8e153c41888c5d21647f2c70
Luis Hernandez <lh1862570@gmail.com>
2025-08-11 16:14:10 -0400

    quite la photo de constellation

--- lib/screens/constellations.dart
+++ lib/screens/constellations.dart
@@ -7,44 +7,11 @@ class ConstellationsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: SafeArea(
-        bottom: false,
-        child: Stack(
-          fit: StackFit.expand,
-          children: [
-            Image.asset('assets/images/constellations.png', fit: BoxFit.cover),
-            const Center(
-              child: Text(
-                'Constelaciones',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: IgnorePointer(
-                child: Container(
-                  height: 120,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Colors.transparent,
-                        Colors.black.withOpacity(0.4),
-                        Colors.black.withOpacity(0.8),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: SafeArea(child: Column(
+        children: [
+          Text('Constelaciones'),
+        ],
+      )),
     );
   }
 }
